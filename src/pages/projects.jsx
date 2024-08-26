2d4268c3276409acba2bbdf8e63ada9ac2d7ef71
UntitledPenguin <qramisw@gmail.com>
2024-08-26 23:26:18 +0100

    modified:   src/pages/contact.jsx 	modified:   src/pages/projects.jsx

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -44,16 +44,7 @@ const Projects = () => {
 						</div>
 
 						<div className="subtitle projects-subtitle">
-							I've worked on a variety of projects over the years
-							and I'm proud of the progress I've made. Many of
-							these projects are open-source and available for
-							others to explore and contribute to. If you're
-							interested in any of the projects I've worked on,
-							please feel free to check out the code and suggest
-							any improvements or enhancements you might have in
-							mind. Collaborating with others is a great way to
-							learn and grow, and I'm always open to new ideas and
-							feedback.
+						Over the years, I've worked on a diverse range of projects, from architectural design and large-scale master planning to machine learning and data analysis. I'm proud of the progress I've made and am eager to share my work. If you're interested in any of my projects, I welcome your suggestions for improvement and am always open to new ideas, constructive feedback, and potential collaboration opportunities.
 						</div>
 
 						<div className="projects-list">
